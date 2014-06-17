cec267c78d990dca74ee7d3ecce34145c585fc8c
JoeChapman <joseph.philip.chapman@gmail.com>
2014-06-17 16:22:11 +0100

    Better form modifications
    - rename forms
    - update package
    - modify common
    - correct indents

--- test/common.js
+++ test/common.js
@@ -2,21 +2,43 @@
 
 global.sinon = require('sinon');
 global.should = require('chai')
-  .use(require('sinon-chai'))
-  .should();
+    .use(require('sinon-chai'))
+    .should();
 
 global.sandbox = function (fn) {
+    var describe = global.describe,
+        beforeAll, afterAll;
 
-  beforeEach(function () {
-    global.sandbox = global.sinon.sandbox.create({
-      injectInto: null,
-      properties: ['spy', 'stub', 'mock']
+    beforeEach(function () {
+        this._sandboxEach = global.sinon.sandbox.create({
+            injectInto: this,
+            properties: ['spy', 'stub', 'mock']
+        });
     });
-  });
 
-  afterEach(function () {
-    global.sandbox.restore();
-  });
+    afterEach(function () {
+        this._sandboxEach.restore();
+    });
 
-  return fn;
+    before(beforeAll = function () {
+        this._sandboxAll = global.sinon.sandbox.create({
+            injectInto: this,
+            properties: ['spy', 'stub', 'mock']
+        });
+    });
+
+    after(afterAll = function () {
+        this._sandboxAll.restore();
+    });
+
+    global.describe = function (string, fn) {
+        describe(string, function () {
+            before(beforeAll);
+            after(afterAll);
+            fn();
+        });
+    };
+
+    return fn;
+
 };
