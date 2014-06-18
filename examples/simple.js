0296356d7d866b94fd00cc324ca37c555eaf8d1f
JoeChapman <joseph.philip.chapman@gmail.com>
2014-06-18 10:57:36 +0100

    Form example
    - modify simple form
    - field factory

--- examples/simple.js
+++ examples/simple.js
@@ -2,8 +2,12 @@
 
 var forms = require('../');
 
-var simpleForm = module.exports = forms('Simple', {
-    name: { required: true, id: 'fname' }
+module.exports = forms('Simple', {
+    name: { required: true, id: 'fname' },
+    addr: forms.fields.email(),
+    pass: forms.fields.password({ id: 'password', required: true }),
+    confirm: { type: 'password', required: true, id: 'confirm' }
+}, {
+    action: '/',
+    method: 'GET'
 });
-
-console.log(simpleForm.fields);
